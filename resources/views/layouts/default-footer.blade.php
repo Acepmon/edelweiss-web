7b6808a8e956372a2beb32e70913c979ea01f218
Tsogtbayar <dtsogtbayar123@gmail.com>
2021-02-05 14:02:03 +0800

    added default layout

---
+++ resources/views/layouts/default-footer.blade.php
@@ -0,0 +1,69 @@
+<!-- Footer -->
+<footer id="footer">
+    <div class="footer-content">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-5">
+                    <div class="widget">
+                        <div class="widget-title">Edelweiss - Flower Shop</div>
+                        <p class="mb-5">Built with love in Ulaanbaatar, Mongolia<br> All rights reserved. Copyright © 2021. Edelweiss.</p>
+                    </div>
+                </div>
+                <div class="col-lg-7">
+                    <div class="row">
+                        <div class="col-lg-3">
+                            <div class="widget">
+                                <div class="widget-title">Discover</div>
+                                <ul class="list">
+                                    <li><a href="#">Features</a></li>
+                                    <li><a href="#">Layouts</a></li>
+                                    <li><a href="#">Corporate</a></li>
+                                    <li><a href="#">Updates</a></li>
+                                    <li><a href="#">Pricing</a></li>
+                                    <li><a href="#">Customers</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-lg-3">
+                            <div class="widget">
+                                <div class="widget-title">Features</div>
+                                <ul class="list">
+                                    <li><a href="#">Layouts</a></li>
+                                    <li><a href="#">Headers</a></li>
+                                    <li><a href="#">Widgets</a></li>
+                                    <li><a href="#">Footers</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-lg-3">
+                            <div class="widget">
+                                <div class="widget-title">Pages</div>
+                                <ul class="list">
+                                    <li><a href="#">Portfolio</a></li>
+                                    <li><a href="#">Blog</a></li>
+                                    <li><a href="#">Shop</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-lg-3">
+                            <div class="widget">
+                                <div class="widget-title">Support</div>
+                                <ul class="list">
+                                    <li><a href="#">Help Desk</a></li>
+                                    <li><a href="#">Documentation</a></li>
+                                    <li><a href="#">Contact Us</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="copyright-content">
+        <div class="container">
+            <div class="copyright-text text-center">&copy; 2021 Edelweiss - Flower Shop. All Rights Reserved.</div>
+        </div>
+    </div>
+</footer>
+<!-- end: Footer -->
